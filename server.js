e58961e3bbca31e0fea185a3f76751f5c8b9ecdf
BadWaka <1456683844@qq.com>
2017-05-18 13:45:35 +0800

    记录请求时间

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-/**
- * require
- */
-const Koa = require('koa');
+/************************ require ************************/
 
-/**
- * initial
- */
+const Koa = require('koa');
+const console = require('tracer').colorConsole();
+
+/************************ initial ************************/
+
 const app = new Koa();
 
-/**
- * middleware
- */
+/************************ middleware ************************/
+
+// req-res time
+app.use(async function (ctx, next) {
+    const startTime = new Date();
+    await next();
+    const ms = new Date() - startTime;
+    console.log(`一次请求时间 req-res time is ${ms}ms`);
+})
+
 app.use(ctx => {
     ctx.body = 'Hello World!';
 });
 
-/**
- * start server
- */
+/************************ start server ************************/
 app.listen(3000);
-console.log('listenning 3000...');
+console.info('listenning 3000...');
