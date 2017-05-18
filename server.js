ea4b87e774bacf4fc5de77672b8186f28a899b41
BadWaka <1456683844@qq.com>
2017-05-18 16:50:05 +0800

    静态目录

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 /************************ require ************************/
 
-const Koa = require('koa');
-const console = require('tracer').colorConsole();
+const Koa = require('koa'); // koa
+const console = require('tracer').colorConsole(); // 增强console
+const koaRouter = require('koa-router')(); // koa-router   设置路由
+const koaStatic = require('koa-static'); // koa-static 设置静态资源
 
 /************************ initial ************************/
 
@@ -9,23 +11,33 @@ const app = new Koa();
 
 /************************ middleware ************************/
 
-// req-res time
+// x-response-time 记录请求时间
 app.use(async function (ctx, next) {
     const startTime = new Date();
     await next();
     const ms = new Date() - startTime;
-    console.log(`一次请求时间 req-res time is ${ms}ms`);
-})
+    ctx.set('X-Response-Time', `${ms}ms`); // 把请求开始到响应完成的时间存到ctx里
+    console.info(`x-response-time ${ms}ms`);
+});
 
-app.use(ctx => {
-    console.debug('headers', ctx.request.headers);
-    ctx.body = 'Hello World!';
+// 设置静态目录
+app.use(koaStatic('static'));
+
+// router 路由
+app.use(koaRouter.routes());
+app.use(koaRouter.allowedMethods());
+
+/************************ router ************************/
+
+koaRouter.get('/index', function (ctx) {
+    console.debug('index', ctx);
 });
 
 /************************ start server ************************/
 
-app.listen(3000);
-console.info('listenning 3000...');
+app.listen(8000);
+``
+console.info('listenning 8000...');
 
 /************************ error handler ************************/
 
