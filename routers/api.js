89e9fbc2713d190f885dbd0f768c549211b41e74
BadWaka <1456683844@qq.com>
2017-05-27 17:17:35 +0800

    async并发

--- routers/api.js
+++ routers/api.js
@@ -4,17 +4,16 @@
 const koaRouter = require('koa-router')(); // koa-router   设置路由
 const console = require('tracer').colorConsole(); // 增强console
 const requestPromise = require('request-promise'); // request请求库的Promise版本
-const cheerio = require('cheerio');
+const cheerio = require('cheerio'); // cheerio 操作 html
 
 /**
  * 必应壁纸爬虫
+ * 
+ * 注：由于该网站的高清图片地址是放在 style 里的，用 cheerio 没法取到，所以这里和 jsdom 配合使用
  */
 koaRouter.get('/api/bingWallPaper', async function (ctx) {
 
     console.debug('/api/bingWallPaper', ctx);
-
-    // 壁纸详情路径地址
-    let wallPaperDetailUrlList = [];
 
     // 请求微软必应壁纸首页，拿到 html
     const bingHost = 'https://bing.ioliu.cn';
@@ -23,28 +22,41 @@ koaRouter.get('/api/bingWallPaper', async function (ctx) {
     // 使用 cheerio 转换
     let $ = cheerio.load(html);
 
+    // 壁纸详情路径地址数组，待会会并发该数组用来获得高清图片地址
+    let wallPaperDetailUrlList = [];
+
     // 过滤壁纸字段
     const masks = $('.mark');
     masks.each((index, item) => {
         item = $(item);
-        const wallPaperUrl = bingHost + item.attr('href');  // 拿到壁纸详情页的地址
+        const wallPaperUrl = bingHost + item.attr('href'); // 拿到壁纸详情页的地址
         wallPaperDetailUrlList.push(wallPaperUrl);
     });
+    console.log('壁纸详情路径地址数组 wallPaperDetailUrlList', wallPaperDetailUrlList);
 
-    console.log('wallPaperList', wallPaperDetailUrlList);
+    // 高清图片地址数组
+    let wallPaperHighDefinitionImgUrlList = [];
 
-    // 并发请求详情页并且取数据
-    wallPaperDetailUrlList.forEach(async(item, index) => {
-        const detailHtml = await requestPromise(item);
-        // console.info('detailHtml', detailHtml);
-        let $ = cheerio.load(detailHtml);
-        const mask = $('.mark');
-        mask.each((index, item) => {
-            // item = $(item);
-            console.info('mask imageUrl', item); // 卡在了无法取得background-image上
+    /**
+     * 获得高清图片地址
+     * @param {*} wallPaperDetailUrlList 
+     */
+    const fetchHighDefinitionImg = async function (wallPaperDetailUrlList) {
+        // 并发请求详情页并且取数据
+        // 注：这里需要注意一个问题，使用 chrome 打开的网页 dom 结构和 cheerio 爬下来的有区别，以 cheerio 的为准
+        wallPaperDetailUrlList.forEach(async(item, index) => {
+            const detailHtml = await requestPromise(item);
+            let $ = cheerio.load(detailHtml);
+            const target = $('.target');
+            const highDefinitionImgUrl = target.attr('data-progressive');
+            wallPaperHighDefinitionImgUrlList.push(highDefinitionImgUrl);
+            console.log('高清图片地址 highDefinitionImgUrl', highDefinitionImgUrl);
         });
+    }
 
-    });
+    fetchHighDefinitionImg(wallPaperDetailUrlList);
+
+    console.log('高清图片地址数组 wallPaperHighDefinitionImgUrlList', wallPaperHighDefinitionImgUrlList);
 
 });
 
