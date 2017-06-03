a10b0fc86c9816c2cae43d94987fada13c24358b
BadWaka <1456683844@qq.com>
2017-06-03 10:02:16 +0800

    准备切换成swig

--- routers/render.js
+++ routers/render.js
@@ -4,6 +4,9 @@
 const koaRouter = require('koa-router')(); // koa-router   设置路由
 const console = require('tracer').colorConsole(); // 增强console
 
+/**
+ * 主页
+ */
 koaRouter.get('/', async function (ctx) {
     console.debug('/', ctx);
     await ctx.render('index', {
@@ -11,8 +14,11 @@ koaRouter.get('/', async function (ctx) {
     });
 });
 
-koaRouter.get('/index', function (ctx) {
-    console.debug('index', ctx);
+/**
+ * 博客页
+ */
+koaRouter.get('/blog', function (ctx) {
+    console.debug('blog', ctx);
 });
 
 module.exports = koaRouter;
