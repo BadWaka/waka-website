2a022db8bc864a1265615b9e588148764c6d28ec
BadWaka <1456683844@qq.com>
2017-06-03 11:32:32 +0800

    分装fileUtil

--- helpBrother/v1.js
+++ helpBrother/v1.js
@@ -1,43 +1,5 @@
-const fs = require('fs');
+const fileUtil = require('../utils/fileUtil');
 const console = require('tracer').colorConsole(); // 增强console
-
-/**
- * 读文件，进行数据处理
- * @return {Promise}
- */
-function readFile(filePath) {
-    return new Promise((resolve, reject) => {
-
-        fs.readFile(filePath, function (err, data) {
-
-            if (err) {
-                console.error(err);
-                reject(err);
-            }
-
-            const dataStr = data.toString();
-            // console.debug('原始数据 dataStr', dataStr);
-
-            const dataArr = dataStr.split('\n');    // 以回车分割
-            // console.debug('以回车分割的数组 dataArr', dataArr);
-
-            const dataStrNoSpaceEnter = dataStr.replace(/\s+/g, '');    // 去除所有空格和回车
-            // console.debug('没有空格和回车的字符串 dataStrNoSpaceEnter', dataStrNoSpaceEnter);
-
-            /**
-             * 进行v1特征分析，返回特征分析结果
-             */
-            const startTime = new Date().getTime();
-            const v1Result = v1(dataStrNoSpaceEnter);
-            const endTime = new Date().getTime();
-            console.debug('v1持续时间', (endTime - startTime) / 1000);
-
-            resolve(v1Result);
-
-        });
-
-    });
-}
 
 /**
  * v1特征分析 是否是韵母：0代表不是，1代表是
@@ -69,26 +31,25 @@ function isVowel(letter, letterIndex, arr) {
     arr[letterIndex] = 0;
 }
 
-/**
- * 写文件，输出csv结果
- * @param data
- * @return {Promise}
- */
-function writeFile(data, outputFilePath) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(outputFilePath, data, function (err) {
-
-            if (err) {
-                console.error(err);
-                reject(err);
-            }
-
-            console.debug('文件写入成功');
-        });
-    });
-}
-
-readFile('./spell.txt')
+fileUtil.readFile('./spell.txt')
     .then(function (data) {
-        writeFile(data, './v1.csv');
+
+        const dataStr = data.toString();
+        // console.debug('原始数据 dataStr', dataStr);
+
+        const dataArr = dataStr.split('\n');    // 以回车分割
+        // console.debug('以回车分割的数组 dataArr', dataArr);
+
+        const dataStrNoSpaceEnter = dataStr.replace(/\s+/g, '');    // 去除所有空格和回车
+        // console.debug('没有空格和回车的字符串 dataStrNoSpaceEnter', dataStrNoSpaceEnter);
+
+        /**
+         * 进行v1特征分析，返回特征分析结果
+         */
+        const startTime = new Date().getTime();
+        const v1Result = v1(dataStrNoSpaceEnter);
+        const endTime = new Date().getTime();
+        console.debug('v1花费时间', (endTime - startTime) / 1000);
+
+        fileUtil.writeFile(v1Result, './v1.csv');
     });
