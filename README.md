ef977dfc79a0b680f61c13c81594a3c96d674d0d
BadWaka <1456683844@qq.com>
2017-06-06 14:56:41 +0800

    去掉swig；使用nunjucks

--- README.md
+++ README.md
@@ -17,7 +17,6 @@
 - nunjucks nunjucks模板引擎
 - request 简化网络请求
 - request-promise 网络请求默认变为promise，方便async/await使用
-- swig swig模板引擎
 - tracer console.log加强
 - ws websocket实现库
 
--- routers/render.js
+++ routers/render.js
@@ -3,55 +3,18 @@
  */
 const koaRouter = require('koa-router')(); // koa-router   设置路由
 const console = require('tracer').colorConsole(); // 增强console
-const swig = require('swig');   // swig模板引擎
 const nunjucks = require('nunjucks');   // nunjucks模板引擎
 const marked = require('marked');   // 解析markdown为html
 const fileUtil = require('../utils/fileUtil');  // 文件工具
 
-function createEnv(path, opts) {
-    let autoescape = opts.autoescape && true,
-        noCache = opts.noCache || false,
-        watch = opts.watch || false,
-        throwOnUndefined = opts.throwOnUndefined || false,
-        env = new nunjucks.Environment(
-            new nunjucks.FileSystemLoader('views', {
-                noCache,
-                watch,
-            }), {
-                autoescape,
-                throwOnUndefined
-            });
-    if (opts.filters) {
-        for (let f in opts.filters) {
-            env.addFilter(f, opts.filters[f]);
-        }
-    }
-    return env;
-}
-
-const env = createEnv('views', {
-    watch: true,
-    filters: {
-        hex: function (n) {
-            return '0x' + n.toString(16);
-        }
-    }
-});
-
 /**
  * 初始化变量
  */
-const viewsPath = process.cwd() + '/views/';
 
 /**
  * 主页
  */
 koaRouter.get('/', async function (ctx) {
-    const template = swig.compileFile(viewsPath + 'index.html');
-    const renderedHtml = template({
-        user: 'waka'
-    });
-    ctx.body = renderedHtml;
 });
 
 /**
