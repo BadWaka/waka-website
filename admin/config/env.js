3a268e9e689011e164dde7a69f860eb584261a56
BadWaka <1456683844@qq.com>
2017-06-08 12:03:00 +0800

    env.js

---
+++ admin/config/env.js
@@ -0,0 +1,52 @@
+/**
+ * 得到客户端的环境变量
+ * @param publicUrl
+ */
+function getClientEnvironment(publicUrl) {
+
+    // 最后得到的 raw 其实是一个对象，里面放了一些服务器的环境变量，应该是为了让浏览器知道当前是什么环境，好做判断，格式为：{ NODE_ENV: 'development', PUBLIC_URL: undefined }
+    const raw = Object.keys(process.env)    // 遍历环境变量名
+        .filter(key => /^REACT_APP_/i.test(key))    // 过滤包含该正则的环境变量名
+        .reduce(
+            // 每个环境变量的值都赋给 env 中去
+            (env, key) => {
+                env[key] = process.env[key];
+                return env;
+            },
+            // 传一个初始对象 env 进去，做为环境
+            {
+                // Useful for determining whether we’re running in production mode.
+                // Most importantly, it switches React into the correct mode.
+                NODE_ENV: process.env.NODE_ENV || 'development',
+                // Useful for resolving the correct path to static assets in `public`.
+                // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+                // This should only be used as an escape hatch. Normally you would put
+                // images into the `src` and `import` them in code to get their paths.
+                PUBLIC_URL: publicUrl,
+            }
+        );
+    // console.log('raw', raw); // 所以最后得到的 raw 其实是一个对象，里面放了一些服务器的环境变量，应该是为了让浏览器知道当前是什么环境，好做判断，格式为：{ NODE_ENV: 'development', PUBLIC_URL: undefined }
+
+    // Stringify all values so we can feed into Webpack DefinePlugin
+    // 对所有值字符串化以便使用 Webpack DefinePlugin 这个插件
+    // 这个插件是允许你创建一个在编译时可以配置的全局常量 http://www.css88.com/doc/webpack2/plugins/define-plugin/
+    // 这个 stringified 得到的值很奇怪，需要这样访问 stringified['process.env']，得到的值类似于这样：{ NODE_ENV: '"development"', PUBLIC_URL: undefined }
+    const stringified = {
+        'process.env': Object.keys(raw)
+            .reduce(
+                (env, key) => {
+                    env[key] = JSON.stringify(raw[key]);
+                    return env;
+                },
+                {}
+            )
+    };
+    // console.log('stringified', stringified['process.env']);
+
+    return {
+        raw,
+        stringified
+    }
+}
+
+module.exports = getClientEnvironment;
--- admin/config/webpack.config.dev.js
+++ admin/config/webpack.config.dev.js
@@ -3,6 +3,7 @@
  */
 const path = require('path');
 const webpack = require('webpack');
+const getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -11,5 +12,5 @@ const publicPath = '/';
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
 const publicUrl = '';
-
-const env = getClientEnvironment();
+// Get environment variables to inject into our app.
+const env = getClientEnvironment(publicUrl);
