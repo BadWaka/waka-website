e22d109a7441cc37345eaf5a8df22fa2eb537a73
BadWaka <1456683844@qq.com>
2017-06-08 16:24:17 +0800

    ts接口

--- typescript-study/3-interface.js
+++ typescript-study/3-interface.js
@@ -1,9 +1,23 @@
-// // /**
-// //  * 接口
-// //  */
-// // // function printLabel(labelledObj: {
-// // //     label: string
-// // // }) {
+// // // /**
+// // //  * 接口
+// // //  */
+// // // // function printLabel(labelledObj: {
+// // // //     label: string
+// // // // }) {
+// // // //     console.log(labelledObj.label);
+// // // // }
+// // // //
+// // // // let myObj = {
+// // // //     size: 10,
+// // // //     label: 'Size 10 Object'
+// // // // };
+// // // // printLabel(myObj);
+// // //
+// // // interface LabelledValue {
+// // //     label: string;
+// // // }
+// // //
+// // // function printLabel(labelledObj: LabelledValue) {
 // // //     console.log(labelledObj.label);
 // // // }
 // // //
@@ -12,25 +26,49 @@
 // // //     label: 'Size 10 Object'
 // // // };
 // // // printLabel(myObj);
+// // //
+// // // // 可选属性
+// // // interface SquareConfig {
+// // //     color?: string,
+// // //     width?: number
+// // // }
+// // //
+// // // function createSquare(config: SquareConfig): { color: string; area: number } {
+// // //     let newSquare = {color: 'white', area: 100};
+// // //     if (config.color) {
+// // //         newSquare.color = config.color;
+// // //     }
+// // //     if (config.width) {
+// // //         newSquare.area = config.width * config.width;
+// // //     }
+// // //     return newSquare;
+// // // }
+// // //
+// // // let mySquare = createSquare({
+// // //     color: 'black'
+// // // });
+// // //
+// // // // 只读属性
+// // // interface Point {
+// // //     readonly x: number;
+// // //     readonly y: number;
+// // // }
+// // //
+// // // let p1: Point = {
+// // //     x: 10,
+// // //     y: 20
+// // // };
+// // // // p1.x = 5;
+// // //
+// // // let a: number[] = [1, 2, 3, 4];
+// // // // let ro: ReadonlyArray<number> = a;
+// // // // ro[0] = 12;
+// // //
 // //
-// // interface LabelledValue {
-// //     label: string;
-// // }
-// //
-// // function printLabel(labelledObj: LabelledValue) {
-// //     console.log(labelledObj.label);
-// // }
-// //
-// // let myObj = {
-// //     size: 10,
-// //     label: 'Size 10 Object'
-// // };
-// // printLabel(myObj);
-// //
-// // // 可选属性
 // // interface SquareConfig {
-// //     color?: string,
-// //     width?: number
+// //     color?: string;
+// //     width?: number;
+// //     [propName: string]: any // 最佳修改方式是添加一个字符串索引签名
 // // }
 // //
 // // function createSquare(config: SquareConfig): { color: string; area: number } {
@@ -44,64 +82,36 @@
 // //     return newSquare;
 // // }
 // //
+// // // 使用类型断言绕过额外属性检查
 // // let mySquare = createSquare({
-// //     color: 'black'
-// // });
-// //
-// // // 只读属性
-// // interface Point {
-// //     readonly x: number;
-// //     readonly y: number;
-// // }
-// //
-// // let p1: Point = {
-// //     x: 10,
-// //     y: 20
+// //     colour: 'red',
+// //     width: 100
+// // } as SquareConfig);
+//
+// // 函数类型
+// interface SearchFunc {
+//     (source: string, subString: string): boolean;
+// }
+//
+// let mySearch: SearchFunc;
+// // mySearch = function (source: string, subString: string) {
+// //     let result = source.search(subString);
+// //     if (result === -1) {
+// //         return true;
+// //     } else {
+// //         return false;
+// //     }
 // // };
-// // // p1.x = 5;
-// //
-// // let a: number[] = [1, 2, 3, 4];
-// // // let ro: ReadonlyArray<number> = a;
-// // // ro[0] = 12;
-// //
 //
-// interface SquareConfig {
-//     color?: string;
-//     width?: number;
-//     [propName: string]: any // 最佳修改方式是添加一个字符串索引签名
-// }
-//
-// function createSquare(config: SquareConfig): { color: string; area: number } {
-//     let newSquare = {color: 'white', area: 100};
-//     if (config.color) {
-//         newSquare.color = config.color;
-//     }
-//     if (config.width) {
-//         newSquare.area = config.width * config.width;
-//     }
-//     return newSquare;
-// }
-//
-// // 使用类型断言绕过额外属性检查
-// let mySquare = createSquare({
-//     colour: 'red',
-//     width: 100
-// } as SquareConfig);
-var mySearch;
-// mySearch = function (source: string, subString: string) {
-//     let result = source.search(subString);
+// mySearch = function (src, sub) {
+//     let result = src.search(sub);
 //     if (result === -1) {
-//         return true;
-//     } else {
 //         return false;
+//     } else {
+//         return true;
 //     }
 // };
-mySearch = function (src, sub) {
-    var result = src.search(sub);
-    if (result === -1) {
-        return false;
-    }
-    else {
-        return true;
-    }
-};
+//
+var myArray;
+myArray = ['Bob', 'Fred'];
+var myStr = myArray[0];
--- typescript-study/3-interface.ts
+++ typescript-study/3-interface.ts
@@ -1,113 +1,128 @@
-// // /**
-// //  * 接口
-// //  */
-// // // function printLabel(labelledObj: {
-// // //     label: string
-// // // }) {
-// // //     console.log(labelledObj.label);
+// // // // /**
+// // // //  * 接口
+// // // //  */
+// // // // // function printLabel(labelledObj: {
+// // // // //     label: string
+// // // // // }) {
+// // // // //     console.log(labelledObj.label);
+// // // // // }
+// // // // //
+// // // // // let myObj = {
+// // // // //     size: 10,
+// // // // //     label: 'Size 10 Object'
+// // // // // };
+// // // // // printLabel(myObj);
+// // // //
+// // // // interface LabelledValue {
+// // // //     label: string;
+// // // // }
+// // // //
+// // // // function printLabel(labelledObj: LabelledValue) {
+// // // //     console.log(labelledObj.label);
+// // // // }
+// // // //
+// // // // let myObj = {
+// // // //     size: 10,
+// // // //     label: 'Size 10 Object'
+// // // // };
+// // // // printLabel(myObj);
+// // // //
+// // // // // 可选属性
+// // // // interface SquareConfig {
+// // // //     color?: string,
+// // // //     width?: number
+// // // // }
+// // // //
+// // // // function createSquare(config: SquareConfig): { color: string; area: number } {
+// // // //     let newSquare = {color: 'white', area: 100};
+// // // //     if (config.color) {
+// // // //         newSquare.color = config.color;
+// // // //     }
+// // // //     if (config.width) {
+// // // //         newSquare.area = config.width * config.width;
+// // // //     }
+// // // //     return newSquare;
+// // // // }
+// // // //
+// // // // let mySquare = createSquare({
+// // // //     color: 'black'
+// // // // });
+// // // //
+// // // // // 只读属性
+// // // // interface Point {
+// // // //     readonly x: number;
+// // // //     readonly y: number;
+// // // // }
+// // // //
+// // // // let p1: Point = {
+// // // //     x: 10,
+// // // //     y: 20
+// // // // };
+// // // // // p1.x = 5;
+// // // //
+// // // // let a: number[] = [1, 2, 3, 4];
+// // // // // let ro: ReadonlyArray<number> = a;
+// // // // // ro[0] = 12;
+// // // //
+// // //
+// // // interface SquareConfig {
+// // //     color?: string;
+// // //     width?: number;
+// // //     [propName: string]: any // 最佳修改方式是添加一个字符串索引签名
 // // // }
 // // //
-// // // let myObj = {
-// // //     size: 10,
-// // //     label: 'Size 10 Object'
+// // // function createSquare(config: SquareConfig): { color: string; area: number } {
+// // //     let newSquare = {color: 'white', area: 100};
+// // //     if (config.color) {
+// // //         newSquare.color = config.color;
+// // //     }
+// // //     if (config.width) {
+// // //         newSquare.area = config.width * config.width;
+// // //     }
+// // //     return newSquare;
+// // // }
+// // //
+// // // // 使用类型断言绕过额外属性检查
+// // // let mySquare = createSquare({
+// // //     colour: 'red',
+// // //     width: 100
+// // // } as SquareConfig);
+// //
+// // // 函数类型
+// // interface SearchFunc {
+// //     (source: string, subString: string): boolean;
+// // }
+// //
+// // let mySearch: SearchFunc;
+// // // mySearch = function (source: string, subString: string) {
+// // //     let result = source.search(subString);
+// // //     if (result === -1) {
+// // //         return true;
+// // //     } else {
+// // //         return false;
+// // //     }
 // // // };
-// // // printLabel(myObj);
 // //
-// // interface LabelledValue {
-// //     label: string;
-// // }
-// //
-// // function printLabel(labelledObj: LabelledValue) {
-// //     console.log(labelledObj.label);
-// // }
-// //
-// // let myObj = {
-// //     size: 10,
-// //     label: 'Size 10 Object'
-// // };
-// // printLabel(myObj);
-// //
-// // // 可选属性
-// // interface SquareConfig {
-// //     color?: string,
-// //     width?: number
-// // }
-// //
-// // function createSquare(config: SquareConfig): { color: string; area: number } {
-// //     let newSquare = {color: 'white', area: 100};
-// //     if (config.color) {
-// //         newSquare.color = config.color;
+// // mySearch = function (src, sub) {
+// //     let result = src.search(sub);
+// //     if (result === -1) {
+// //         return false;
+// //     } else {
+// //         return true;
 // //     }
-// //     if (config.width) {
-// //         newSquare.area = config.width * config.width;
-// //     }
-// //     return newSquare;
-// // }
-// //
-// // let mySquare = createSquare({
-// //     color: 'black'
-// // });
-// //
-// // // 只读属性
-// // interface Point {
-// //     readonly x: number;
-// //     readonly y: number;
-// // }
-// //
-// // let p1: Point = {
-// //     x: 10,
-// //     y: 20
 // // };
-// // // p1.x = 5;
-// //
-// // let a: number[] = [1, 2, 3, 4];
-// // // let ro: ReadonlyArray<number> = a;
-// // // ro[0] = 12;
 // //
 //
-// interface SquareConfig {
-//     color?: string;
-//     width?: number;
-//     [propName: string]: any // 最佳修改方式是添加一个字符串索引签名
+// // 可索引的数据类型
+// interface StringArray {
+//     [index: number]: string;
 // }
 //
-// function createSquare(config: SquareConfig): { color: string; area: number } {
-//     let newSquare = {color: 'white', area: 100};
-//     if (config.color) {
-//         newSquare.color = config.color;
-//     }
-//     if (config.width) {
-//         newSquare.area = config.width * config.width;
-//     }
-//     return newSquare;
-// }
+// let myArray: StringArray;
+// myArray = ['Bob', 'Fred'];
 //
-// // 使用类型断言绕过额外属性检查
-// let mySquare = createSquare({
-//     colour: 'red',
-//     width: 100
-// } as SquareConfig);
+// let myStr: string = myArray[0];
 
-// 函数类型
-interface SearchFunc {
-    (source: string, subString: string): boolean;
+class Animal {
+
 }
-
-let mySearch: SearchFunc;
-// mySearch = function (source: string, subString: string) {
-//     let result = source.search(subString);
-//     if (result === -1) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// };
-
-mySearch = function (src, sub) {
-    let result = src.search(sub);
-    if (result === -1) {
-        return false;
-    } else {
-        return true;
-    }
-};
