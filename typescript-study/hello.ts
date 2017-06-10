15709afe02c9cd06db082b2b88e2954fb317e057
BadWaka <1456683844@qq.com>
2017-06-10 23:14:23 +0800

    元祖

--- typescript-study/hello.ts
+++ typescript-study/hello.ts
@@ -1,32 +1,42 @@
-// function sayHello(person: string) {
-//     return 'Hello, ' + person;
-// }
-// let user = 'waka';
-// console.log(sayHello(user));
+// // function sayHello(person: string) {
+// //     return 'Hello, ' + person;
+// // }
+// // let user = 'waka';
+// // console.log(sayHello(user));
+// //
+// // let my: string | number | boolean;
+// // my = 'seven';
+// // my = 7;
+// // my = true;
+// //
+// // function getLength(something: string | number): string {
+// //     return something.toString();
+// // }
+// //
+// // interface Person {
+// //     name: string;
+// //     age?: number;
+// //     [propName: string]: any;
+// // }
+// // interface NumberArray {
+// //     [index: number]: number;
+// // }
+// // let fi: NumberArray = [1, 2, 3, 4, 5];
 //
-// let my: string | number | boolean;
-// my = 'seven';
-// my = 7;
-// my = true;
-//
-// function getLength(something: string | number): string {
-//     return something.toString();
+// type EventName = 'click' | 'scroll' | 'mousemove';
+// function handleEvent(ele: Element, event: EventName) {
+//     // do something
 // }
 //
-// interface Person {
-//     name: string;
-//     age?: number;
-//     [propName: string]: any;
-// }
-// interface NumberArray {
-//     [index: number]: number;
-// }
-// let fi: NumberArray = [1, 2, 3, 4, 5];
+// handleEvent(document.getElementById('hello'), 'scroll');
+// handleEvent(document.getElementById('world'), 'dbclick');
 
-type EventName = 'click' | 'scroll' | 'mousemove';
-function handleEvent(ele: Element, event: EventName) {
-    // do something
-}
+let x: [string, number];
+// x[0] = 'waka';
+// x[1] = 25;
+//
+// x[0].slice(1);
+// x[1].toFixed(2);
 
-handleEvent(document.getElementById('hello'), 'scroll');
-handleEvent(document.getElementById('world'), 'dbclick');
+x = ['waka', 1, '1111111111'];
+console.log(x[2].slice(1));
