7fc260102b2b04ed87b84fa8514fdeacf716ab82
BadWaka <1456683844@qq.com>
2017-06-10 23:04:21 +0800

    老小雨的健身计划

--- typescript-study/hello.ts
+++ typescript-study/hello.ts
@@ -1,14 +1,30 @@
-function sayHello(person: string) {
-    return 'Hello, ' + person;
-}
-let user = 'waka';
-console.log(sayHello(user));
+// function sayHello(person: string) {
+//     return 'Hello, ' + person;
+// }
+// let user = 'waka';
+// console.log(sayHello(user));
+//
+// let my: string | number | boolean;
+// my = 'seven';
+// my = 7;
+// my = true;
+//
+// function getLength(something: string | number): string {
+//     return something.toString();
+// }
+//
+// interface Person {
+//     name: string;
+//     age?: number;
+//     [propName: string]: any;
+// }
+// interface NumberArray {
+//     [index: number]: number;
+// }
+// let fi: NumberArray = [1, 2, 3, 4, 5];
 
-let my: string | number | boolean;
-my = 'seven';
-my = 7;
-my = true;
-
-function getLength(something: string | number): string {
-    return something.toString();
+type EventName = 'click' | 'scroll' | 'mousemove';
+function handleEvent(ele: Element, event: EventName) {
+    // do something
 }
+
