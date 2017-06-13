c8430d89d6150dcb86cfb8ed5ef73eb927f35c48
BadWaka <1456683844@qq.com>
2017-06-13 11:32:55 +0800

    初始化工程完毕

--- admin/src/index.js
+++ admin/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // reset.css
 import './common/reset.scss';
 
+// antd less
 import 'antd/dist/antd.less';
 
 // components
@@ -12,22 +13,9 @@ import {
     Header
 } from './components/index';
 
-// test antd
-import {
-    Button,
-    DatePicker
-} from 'antd';
-
 ReactDOM.render(
     <div>
         <Header/>
-        <div>
-            <Button type="primary">Primary</Button>
-            <Button>Default</Button>
-            <Button type="dashed">Dashed</Button>
-            <Button type="danger">Danger</Button>
-        </div>
-        <DatePicker/>
         <Footer/>
     </div>,
     document.getElementById('root')
