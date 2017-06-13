269fd78a202629dd8cdedde2930c8f88275ed27a
BadWaka <1456683844@qq.com>
2017-06-13 15:50:47 +0800

    观察者模式实践

--- admin/src/index.js
+++ admin/src/index.js
@@ -9,18 +9,40 @@ const appState = {
     }
 };
 
-function dispatch(action) {
+function stateChanger(state, action) {
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text;
+            state.title.text = action.text;
             break;
         case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color;
+            state.title.color = action.color;
             break;
         default:
             break;
     }
 }
+
+function createStore(state, stateChanger) {
+    const listeners = [];
+    const subscribe = (listener) => listeners.push(listener);
+    const getState = () => state;
+    const dispatch = (action) => {
+        stateChanger(state, action);
+        listeners.forEach((listener) => {
+            listener();
+        });
+    };
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+}
+
+const store = createStore(appState, stateChanger);
+store.subscribe(() => {
+    renderApp(store.getState());
+});
 
 function renderTitle(title) {
     const titleDOM = document.getElementById('title');
@@ -39,9 +61,7 @@ function renderApp(appState) {
     renderContent(appState.content);
 }
 
-renderApp(appState);
+renderApp(store.getState());
 
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
-dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
-
-renderApp(appState);
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'}); // 修改标题文本
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'}); // 修改标题颜色
