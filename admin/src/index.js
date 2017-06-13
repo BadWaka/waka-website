abc9ab9d900e9785728ed741afcda1362c7958c1
BadWaka <1456683844@qq.com>
2017-06-13 19:00:13 +0800

    移除目录

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,66 +1,80 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom'
-import Header from './Header'
-import Content from './Content'
+/************************ React ****************************/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/************************ React Router ****************************/
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+/************************ Redux ****************************/
+
+import {
+    createStore
+} from 'redux';
+
 import {
     Provider
-} from './react-redux';
+} from 'react-redux'
 
-// 生成 store
-function createStore(reducer) {
-    let state = null;
-    const listeners = [];
-    const subscribe = (listener) => listeners.push(listener);
-    const getState = () => state;
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach((listener) => {
-            listener();
-        });
-    };
-    dispatch({});   // 初始化 state
-    return {
-        getState,
-        dispatch,
-        subscribe
+/************************ stylesheet ****************************/
+
+// reset.css
+import './common/reset.scss';
+
+// antd less
+import 'antd/dist/antd.less';
+
+/************************ Material UI ****************************/
+
+// 使用 react-tap-event-plugin 监听 touch / tap / clickevents 事件，移动端使用 onTouchTap 事件替换 onClick 事件 http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+// Theme
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';    // 黑暗主题
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // 主题提供器组件
+import getMuiTheme from 'material-ui/styles/getMuiTheme';   // 获得主题的方法
+import _colors from './common/color';   // 颜色js
+
+// 自定义主题
+const muiTheme = getMuiTheme({
+    // 调色板
+    palette: {
+        primary1Color: _colors.lightBlue500,
+        primary2Color: _colors.lightBlue500,
+        primary3Color: _colors.grey600,
+        accent1Color: _colors.pinkA200,
+        accent2Color: _colors.pinkA400,
+        accent3Color: _colors.pinkA100,
+        textColor: _colors.fullWhite,
     }
-}
+});
 
-const themeReducer = (state, action) => {
-    if (!state) {
-        return {
-            themeColor: 'red'
-        }
-    }
-    switch (action.type) {
-        case 'CHANGE_COLOR':
-            return {
-                ...state,
-                themeColor: action.themeColor
-            };
-        default:
-            return state;
-    }
-};
+/************************ Components ****************************/
 
-const store = createStore(themeReducer);
+import {
+    Header,
+    Footer
+} from './containers';
 
-class Index extends Component {
-
-    render() {
-        return (
-            <div>
-                <Header />
-                <Content />
-            </div>
-        )
-    }
-}
+/************************ render ****************************/
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Index/>
-    </Provider>,
+    // react-router 路由
+    <Router history="">
+        {/* MaterialUI 主题 */}
+        <MuiThemeProvider muiTheme={muiTheme}>
+            {/* 主体 */}
+            <section>
+                <Header title='waka'/>
+                <Footer/>
+            </section>
+        </MuiThemeProvider>
+    </Router>,
     document.getElementById('root')
 );
