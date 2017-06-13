4570c9a9a8e6e0f266486880fc8322bb50ad6feb
BadWaka <1456683844@qq.com>
2017-06-13 14:29:09 +0800

    集成react-router

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,5 +1,17 @@
+/************************ React ****************************/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+/************************ React Router ****************************/
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+/************************ stylesheet ****************************/
 
 // reset.css
 import './common/reset.scss';
@@ -7,43 +19,46 @@ import './common/reset.scss';
 // antd less
 import 'antd/dist/antd.less';
 
+/************************ Material UI ****************************/
+
 // 使用 react-tap-event-plugin 监听 touch / tap / clickevents 事件，移动端使用 onTouchTap 事件替换 onClick 事件 http://stackoverflow.com/a/34015469/988941
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-// Material UI
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';    // 黑暗主题
+// Theme
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';    // 黑暗主题
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // 主题提供器组件
 import getMuiTheme from 'material-ui/styles/getMuiTheme';   // 获得主题的方法
-import AppBar from 'material-ui/AppBar';
-import _colors from './common/color';
+import _colors from './common/color';   // 颜色js
+
 // 自定义主题
 const muiTheme = getMuiTheme({
     // 调色板
     palette: {
-        primary1Color: _colors.lightBlue700,
+        primary1Color: _colors.lightBlue500,
+        primary2Color: _colors.lightBlue500,
+        primary3Color: _colors.grey600,
+        accent1Color: _colors.pinkA200,
+        accent2Color: _colors.pinkA400,
+        accent3Color: _colors.pinkA100,
+        textColor: _colors.fullWhite,
     }
 });
 
-// components
-import {
-    Footer,
-    Header
-} from './components';
+/************************ Components ****************************/
 
-// container
-import {
-    EditArticle
-} from './containers';
+// material-ui
+import AppBar from 'material-ui/AppBar';
 
-const App = () => {
-    return <MuiThemeProvider muiTheme={muiTheme}>
-        <AppBar title="My AppBar"/>
-    </MuiThemeProvider>;
-};
-
+/************************ render ****************************/
 
 ReactDOM.render(
-    <App/>,
+    <Router history="">
+        <MuiThemeProvider muiTheme={muiTheme}>
+            <section>
+                <AppBar/>
+            </section>
+        </MuiThemeProvider>
+    </Router>,
     document.getElementById('root')
 );
