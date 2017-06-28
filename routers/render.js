3d1da5ef530a23d14cb31be6e64a2a480526504d
BadWaka <1456683844@qq.com>
2017-06-28 18:22:42 +0800

    nunjucks渲染对象和字符串

--- routers/render.js
+++ routers/render.js
@@ -6,17 +6,29 @@ const console = require('tracer').colorConsole(); // 增强console
 const nunjucks = require('nunjucks');   // nunjucks模板引擎
 const marked = require('marked');   // 解析markdown为html
 const fileUtil = require('../utils/fileUtil');  // 文件工具
+const requestPromise = require('request-promise');  // 网络请求
 
 /**
  * 初始化变量
  */
+const currentHost = 'http://localhost:5000';
 
 /**
  * 主页
  */
 koaRouter.get('/', async function (ctx) {
-    
-    ctx.render('index.html');
+    // 请求所有文章数据
+    const p1 = requestPromise(currentHost + '/api/getArticle');
+    try {
+        const values = await Promise.all([p1]);
+        console.debug('values', values);
+        ctx.render('index.html', {
+            articles: JSON.parse(values[0]),
+            articlesStr: encodeURIComponent(values[0])
+        });
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 /**
