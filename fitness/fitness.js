236ccafb3f082a8acc518452371ae8c5a8a9de57
BadWaka <1456683844@qq.com>
2017-08-16 23:34:31 +0800

    2017-08-16

--- fitness/fitness.js
+++ fitness/fitness.js
@@ -1,6 +1,9 @@
 const fitness = {
     array:[{
-        date:'20170723',
+        date:'2017-07-23',
         weight:156,
+    },{
+        date:'2017-08-16',
+        content:'5x5 第一天 深蹲30kg 卧推30kg 哑铃划船30kg'
     }]
 };
